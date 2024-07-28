672835771eb2ff0d7722114d4c8319b4891da1c5
shayanabedi98 <shayanabedi@hotmail.com>
2024-07-28 19:00:10 -0400

    initial commit

---
+++ app/components/nav/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Container from "../Container";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
+
+const Navbar = () => {
+  return (
+    <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm ">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between md:gap-0 gap-3">
+            <Link
+              className={`${redressed.className} font-bold text-2xl`}
+              href={"/"}
+            >
+              E-Shop
+            </Link>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div>CartCount</div>
+              <div>UserMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
